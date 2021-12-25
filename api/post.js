1037eff0b68e3879c3b89210f2b74b37486653a1
Brennan Colberg <brennan.colberg@gmail.com>
2021-12-24 23:30:14 -0800

    algorithm todos

--- api/post.js
+++ api/post.js
@@ -5,6 +5,11 @@ export default (req, res) => {
   // request must be authenticated
   if (req.headers["x-api-key"] !== process.env.API_KEY) return res.status(401).end()
 
+  // TODO get current day
+  // TODO figure out how many days there are until next Christmas
+  // TODO see how many tweets will be needed to fit all "Eve"s
+  // TODO send all tweets, reply-chained into a thread
+
   // TODO change to 201 once implemented
   res.statusCode(501).end()
 }
